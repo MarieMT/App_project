49d74b6fd4c6a58b936e706e0ac390dea016039b
MarieMT <mtk@live.de>
2016-07-17 22:05:29 +0000

    Validation conditions

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
     belongs_to :user
-    validates :name, presence: true
+    validates :name, presence: true && :name, length: { minimum: 4 }
+    validates :address, presence: true
+    validates :description, presence: true
 end
